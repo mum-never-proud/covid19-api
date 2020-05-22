fa944340354d1e4d1000a017a764f4f4acf5afd9
mum-never-proud <abhuz@hotmail.com>
2020-05-22 21:36:35 +0530

    add server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('hello covid19 api');
+import Express from 'express';
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = new Express();
+const { APP_NAME, APP_PORT } = process.env;
+
+// eslint-disable-next-line no-console
+app.listen(APP_PORT, () => console.log(chalk`{blue.bold ${APP_NAME} started on ${APP_PORT} @ ${Date.now()}}`));
